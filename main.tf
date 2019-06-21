47b3fdfdd38de5819b9523640a0d977c5ad805ea
ghxops <51935312+ghxops@users.noreply.github.com>
2019-06-21 21:21:55 +0000

    Initial commit

---
+++ main.tf
@@ -0,0 +1,25 @@
+provider "aws" {
+  region = "${var.aws_region}"
+}
+
+resource "aws_elb" "web-elb" {
+  name = "terraform-example-elb"
+
+  # The same availability zone as our instances
+  availability_zones = ["${split(",", var.availability_zones)}"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:80/"
+    interval            = 30
+  }
+}
